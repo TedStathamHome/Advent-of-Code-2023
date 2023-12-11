32b4e37e41d2d6b84a0edb58b8667d7fccb8c55d
Ted Statham <65921212+TedStathamHome@users.noreply.github.com>
2023-12-10 22:07:45 -0700

    Wrote brute force of day 8 part B
    Need an alternate way as it'll take a very long time to step through them all

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -12,14 +12,15 @@ namespace Day08
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2023: Day 8");
-            var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
+            var isInTestMode = (args.Length > 0 && args[0].Trim().ToLower() == "test");
+            var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{(isInTestMode ? "test" : "full")}.txt").ToList();
 
             NavigationInstructions = puzzleInputRaw[0];
             ParsePuzzleInput(puzzleInputRaw);
 
             Console.WriteLine($"* Network nodes: {NetworkNodes.Count:N0}; navigation instructions {NavigationInstructions.Length:N0}");
 
-            PartA();
+            // PartA();
             PartB();
         }
 
@@ -63,6 +64,51 @@ namespace Day08
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part B");
+            
+            var step = 0;
+            bool doAllCurrentNodesEndInZ = false;
+            var startingNodes = NetworkNodes.Where(n => n.Value.EndsInA).Select(n => n.Value).ToList();
+
+            Console.WriteLine($"** Nodes ending in A: {startingNodes.Count:N0}");
+            
+            // probably do a single pass for each to figure out how many steps it
+            // takes to get to Z and then figure out a multiplier to get them all
+            // to the same place
+            
+            while (!doAllCurrentNodesEndInZ)
+            {
+                foreach (var instruction in NavigationInstructions)
+                {
+                    doAllCurrentNodesEndInZ = true;
+                    step++;
+
+                    for (int n = 0; n < startingNodes.Count; n++)
+                    {
+                        var currentNode = startingNodes[n].CurrentNode;
+                        var node = NetworkNodes[currentNode];
+
+                        var nextNode = instruction == 'L'
+                            ? NetworkNodes[currentNode].NodeToLeft 
+                            : NetworkNodes[currentNode].NodeToRight;
+
+                        startingNodes[n].CurrentNode = nextNode;
+
+                        doAllCurrentNodesEndInZ = doAllCurrentNodesEndInZ && NetworkNodes[nextNode].EndsInZ;
+                    }
+
+                    if (doAllCurrentNodesEndInZ)
+                    {
+                        break;
+                    }
+
+                    if (step % 1000 == 0)
+                    {
+                        Console.WriteLine($"** Steps: {step:N0}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"** Took {step:N0} step(s) to get to all nodes ending in Z.");
         }
     }
 
@@ -71,6 +117,9 @@ namespace Day08
         public string NodeID { get; set; }
         public string NodeToLeft { get; set; }
         public string NodeToRight { get; set; }
+        public string CurrentNode { get; set; }
+        public bool EndsInA { get; set; }
+        public bool EndsInZ { get; set; }
 
         public NetworkNode(string rawNodeDetails)
         {
@@ -78,6 +127,9 @@ namespace Day08
             NodeID = details[0];
             NodeToLeft = details[1];
             NodeToRight = details[2];
+            EndsInA = NodeID[^1] == 'A';
+            EndsInZ = NodeID[^1] == 'Z';
+            CurrentNode = NodeID;
         }
     }
 }
