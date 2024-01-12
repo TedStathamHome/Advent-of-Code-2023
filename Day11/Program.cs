4134ae025fd18a37948588eef05ce24dead3b0a2
Ted Statham <TStatham@local.sequiter.com>
2024-01-12 15:21:36 -0700

    Day 11, refactoring and optimizations for part B

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -8,29 +8,61 @@ namespace Day11
     {
 		private const string EmptySpace = ".";
 		private const string Galaxy = "#";
+		// private const int ExpansionRate = 2;
+		private const int ExpansionRate = 1_000_000;
+		private static List<int> ColsToExpand;
+		private	static List<int> RowsToExpand;
 
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Advent of Code 2023: Day 11");
 			var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
-			var expandedUniverse = ExpandTheUniverse(puzzleInputRaw);
-			var galaxyCoordinates = FindTheGalaxies(expandedUniverse);
+			ColsToExpand = FindEmptyColumns(puzzleInputRaw);
+			RowsToExpand = FindEmptyRows(puzzleInputRaw);
+			var galaxyCoordinates = FindTheGalaxies(puzzleInputRaw);
 			var galaxyPairs = BuildGalaxyPairs(galaxyCoordinates);
 
 			Console.WriteLine($"Original universe is {puzzleInputRaw[0].Length:N0} x {puzzleInputRaw.Count:N0}");
+			Console.WriteLine($"Expanded universe is {puzzleInputRaw[0].Length + (ColsToExpand.Count * (ExpansionRate - 1)):N0} x {puzzleInputRaw.Count + (RowsToExpand.Count * (ExpansionRate - 1)):N0}");
 			Console.WriteLine($"Number of galaxies: {galaxyCoordinates.Count:N0}");
-			Console.WriteLine($"Expanded universe is {expandedUniverse[0].Length:N0} x {expandedUniverse.Count:N0}");
-
-			for (int i = 0; i < expandedUniverse.Count; i++)
-			{
-				Console.WriteLine(expandedUniverse[i]);
-			}
-
+			
 			PartA(galaxyPairs);
 			PartB();
 		}
 
-        private static void PartA(List<GalaxyPair> galaxyPairs)
+		private static List<int> FindEmptyColumns(List<string> universe)
+		{
+			var emptyCols = new List<int>();
+
+			for (var c = 0; c < universe[0].Length; c++)
+			{
+				var universeColumn = string.Join("", universe.Select(l => l[c]).ToList());
+
+				if (universeColumn.Replace(EmptySpace, "").Length == 0)
+				{
+					emptyCols.Add(c);
+				}
+			}
+
+			return emptyCols;
+		}
+
+		private static List<int> FindEmptyRows(List<string> universe)
+		{
+			var emptyRows = new List<int>();
+
+			for (var r = 0; r < universe.Count; r++)
+			{
+				if (universe[r].Replace(EmptySpace, "").Length == 0)
+				{
+					emptyRows.Add(r);
+				}
+			}
+
+			return emptyRows;
+		}
+
+		private static void PartA(List<GalaxyPair> galaxyPairs)
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part A");
@@ -41,12 +73,12 @@ namespace Day11
 
 			foreach (var galaxyPair in galaxyPairs)
 			{
-				Console.WriteLine($"*** {i:N0}: {galaxyPair.GalaxyA} -> {galaxyPair.GalaxyB} = {galaxyPair.PathLength:N0}");
+				Console.WriteLine($"*** {i:N0}: {galaxyPair.GalaxyA} -> {galaxyPair.GalaxyB} = {galaxyPair.PathLength:N0} (iterations: {galaxyPair.Iterations:N0})");
 				i++;
 			}
 
-			var sumOfShortestPaths = galaxyPairs.Sum(gp => gp.PathLength);
-			Console.WriteLine($"** Sum of shortest paths: {sumOfShortestPaths:N0}");
+			//var sumOfShortestPaths = galaxyPairs.Sum(gp => (ulong) gp.PathLength);
+			//Console.WriteLine($"** Sum of shortest paths: {sumOfShortestPaths:N0}");
 		}
 
 		private static void PartB()
@@ -54,36 +86,6 @@ namespace Day11
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part B");
         }
-
-		private static List<string> ExpandTheUniverse(List<string> originalUniverse)
-		{
-			var expandedUniverse = originalUniverse.ToList();
-
-			// expand the universe vertically, from the bottom up
-			for (var i = expandedUniverse.Count - 1; i >= 0; i--)
-			{
-				if (expandedUniverse[i].Replace(EmptySpace, "").Length == 0)
-				{
-					expandedUniverse.Insert(i, expandedUniverse[i]);
-				}
-			}
-
-			// expand the universe horizontally, from right to left
-			for (var i = expandedUniverse[0].Length - 1; i >= 0; i--)
-			{
-				var galaxyColumn = string.Join("", expandedUniverse.Select(x => x[i]).ToList());
-				
-				if (galaxyColumn.Replace(EmptySpace, "").Length == 0)
-				{
-					for (var j = 0; j < expandedUniverse.Count; j++)
-					{
-						expandedUniverse[j] = expandedUniverse[j].Insert(i, EmptySpace);
-					}
-				}
-			}
-
-			return expandedUniverse;
-		}
 
 		private static List<GalaxyCoordinate> FindTheGalaxies(List<string> universe)
 		{
@@ -109,6 +111,7 @@ namespace Day11
 		private static List<GalaxyPair> BuildGalaxyPairs(List<GalaxyCoordinate> galaxyCoordinates)
 		{
 			var galaxyPairs = new List<GalaxyPair>();
+			var count = 0;
 
 			for (var i = 0; i < galaxyCoordinates.Count - 1; i++)
 			{
@@ -116,52 +119,107 @@ namespace Day11
 				{
 					var pathLength = PathLengthBetweenGalaxies(galaxyCoordinates[i], galaxyCoordinates[j]);
 
-					galaxyPairs.Add(new GalaxyPair() { GalaxyA = i, GalaxyB = j, PathLength = pathLength }); 
+					galaxyPairs.Add(new GalaxyPair() { GalaxyA = i, GalaxyB = j, PathLength = pathLength.PathLength, Iterations = pathLength.Iterations });
+					
+					count++;
+					if (count > 1000) break;
 				}
+
+				if (count > 1000) break;
 			}
 
 			return galaxyPairs;
 		}
 
-		private static int PathLengthBetweenGalaxies(GalaxyCoordinate galaxyA, GalaxyCoordinate galaxyB)
+		private static (ulong PathLength, int Iterations) PathLengthBetweenGalaxies(GalaxyCoordinate galaxyA, GalaxyCoordinate galaxyB)
 		{
+			int emptyColsBetweenGalaxies = 0;
+			// if galaxy A is to the left of galaxy B by more than 1 column, look for the empty columns between them
+			if (galaxyA.Column < galaxyB.Column && (galaxyB.Column - galaxyA.Column) > 1)
+			{
+				emptyColsBetweenGalaxies = ColsToExpand.Where(c => c > galaxyA.Column && c < galaxyB.Column).Count();
+			}
+			// if galaxy A is to the right of galaxy B by more than 1 column, look for the empty columns between them
+			else if (galaxyA.Column > galaxyB.Column && (galaxyA.Column - galaxyB.Column) > 1)
+			{
+				emptyColsBetweenGalaxies = ColsToExpand.Where(c => c > galaxyB.Column && c < galaxyA.Column).Count();
+			}
+
 			// this can be zero (same column), positive (to the right) or negative (to the left)
 			var xDistance = galaxyB.Column - galaxyA.Column;
-			
+
+			// adjust for the extra space needed for galactic expansion
+			xDistance += ((xDistance >= 0) ? 1 : -1) * emptyColsBetweenGalaxies * (ExpansionRate - 1);
+
+
 			// this will always be zero (same row) or positive (below)
 			var yDistance = galaxyB.Row - galaxyA.Row;
 
+			int emptyRowsBetweenGalaxies = 0;
+			// if galaxy B is below galaxy A by more than 1 row, look for the empty rows between them
+			if (yDistance > 1)
+			{
+				emptyRowsBetweenGalaxies = RowsToExpand.Where(r => r > galaxyA.Row && r < galaxyB.Row).Count();
+			}
+
+			// adjust for the extra space needed for galactic expansion
+			yDistance += emptyRowsBetweenGalaxies * (ExpansionRate - 1);
+
+
 			// if both galaxies are on the same column, the shortest path is a vertical straight line
-			if (xDistance == 0) return int.Abs(yDistance);
+			if (xDistance == 0) return ((ulong) int.Abs(yDistance), 1);
 
 			// if both galaxies are on the same row, the shortest path is a horizontal straight line
-			if (yDistance == 0) return int.Abs(xDistance);
+			if (yDistance == 0) return ((ulong) int.Abs(xDistance), 1);
 
-			var pathLength = 0;
+			ulong pathLength = 0;
+			var iterations = 0;
+
+			// need to optimize this for handling long distance differences
 
 			// loop until there is no more x or y distance remaining
 			while (xDistance != 0 || yDistance != 0)
 			{
-				pathLength++;
+				// pathLength++;
+				iterations++;
 				var xChange = 0;
 				var yChange = 0;
 
 				// if we have farther to go (or the same) on the x axis, move on the x axis
 				if (int.Abs(xDistance) >= int.Abs(yDistance))
 				{
-					xChange = xDistance > 0 ? -1 : 1;
+					var relativeDistanceDiff = (int.Abs(xDistance) - int.Abs(yDistance));
+					
+					// if the x and y distances are the same, move at least one space on the x axis
+					if (relativeDistanceDiff == 0)
+					{
+						xChange = xDistance > 0 ? -1 : 1;
+					}
+					else
+					{
+						// otherwise, move as many steps as it takes to make the x and y distances equal
+						xChange = (xDistance > 0 ? -1 : 1) * relativeDistanceDiff;
+					}
+
+					pathLength += (ulong) int.Abs(xChange);
 				}
 				else
 				{
 					// we have farther to go on the y axis
-					yChange = yDistance > 0 ? -1 : 1;
+					// this should always be at least 1
+					var relativeDistanceDiff = (int.Abs(yDistance) - int.Abs(xDistance));
+
+					// move as many steps as it takes to make the y and x distances equal
+					yChange = (yDistance > 0 ? -1 : 1) * relativeDistanceDiff;
+
+					pathLength += (ulong) int.Abs(yChange);
 				}
 
 				xDistance += xChange;
 				yDistance += yChange;
 			}
 
-			return pathLength;
+			return (pathLength, iterations);
 		}
 	}
 
@@ -175,6 +233,7 @@ namespace Day11
 	{
 		public int GalaxyA { get; set; }
 		public int GalaxyB { get; set; }
-		public int PathLength { get; set; }
+		public ulong PathLength { get; set; }
+		public int Iterations { get; set; }
 	}
 }
