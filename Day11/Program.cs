2dff51ccb36a71febf99a66ebcf17488b3a22de6
Ted Statham <TStatham@local.sequiter.com>
2024-01-12 10:22:50 -0700

    Complated day 11, part A

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -15,6 +15,7 @@ namespace Day11
 			var puzzleInputRaw = File.ReadLines($"./PuzzleInput-{((args.Length > 0 && args[0].Trim().ToLower() == "test") ? "test" : "full")}.txt").ToList();
 			var expandedUniverse = ExpandTheUniverse(puzzleInputRaw);
 			var galaxyCoordinates = FindTheGalaxies(expandedUniverse);
+			var galaxyPairs = BuildGalaxyPairs(galaxyCoordinates);
 
 			Console.WriteLine($"Original universe is {puzzleInputRaw[0].Length:N0} x {puzzleInputRaw.Count:N0}");
 			Console.WriteLine($"Number of galaxies: {galaxyCoordinates.Count:N0}");
@@ -25,17 +26,30 @@ namespace Day11
 				Console.WriteLine(expandedUniverse[i]);
 			}
 
-			PartA();
+			PartA(galaxyPairs);
 			PartB();
 		}
 
-        private static void PartA()
+        private static void PartA(List<GalaxyPair> galaxyPairs)
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part A");
-        }
+			Console.WriteLine($"** Number of galaxy pairs: {galaxyPairs.Count:N0}");
+			Console.WriteLine("*** Distances between galaxies:");
 
-        private static void PartB()
+			var i = 0;
+
+			foreach (var galaxyPair in galaxyPairs)
+			{
+				Console.WriteLine($"*** {i:N0}: {galaxyPair.GalaxyA} -> {galaxyPair.GalaxyB} = {galaxyPair.PathLength:N0}");
+				i++;
+			}
+
+			var sumOfShortestPaths = galaxyPairs.Sum(gp => gp.PathLength);
+			Console.WriteLine($"** Sum of shortest paths: {sumOfShortestPaths:N0}");
+		}
+
+		private static void PartB()
         {
             Console.WriteLine("\r\n**********");
             Console.WriteLine("* Part B");
@@ -111,12 +125,10 @@ namespace Day11
 
 		private static int PathLengthBetweenGalaxies(GalaxyCoordinate galaxyA, GalaxyCoordinate galaxyB)
 		{
-			var pathLength = 0;
-			
 			// this can be zero (same column), positive (to the right) or negative (to the left)
 			var xDistance = galaxyB.Column - galaxyA.Column;
 			
-			// this will always be zero (same row) or greater (below)
+			// this will always be zero (same row) or positive (below)
 			var yDistance = galaxyB.Row - galaxyA.Row;
 
 			// if both galaxies are on the same column, the shortest path is a vertical straight line
@@ -124,6 +136,30 @@ namespace Day11
 
 			// if both galaxies are on the same row, the shortest path is a horizontal straight line
 			if (yDistance == 0) return int.Abs(xDistance);
+
+			var pathLength = 0;
+
+			// loop until there is no more x or y distance remaining
+			while (xDistance != 0 || yDistance != 0)
+			{
+				pathLength++;
+				var xChange = 0;
+				var yChange = 0;
+
+				// if we have farther to go (or the same) on the x axis, move on the x axis
+				if (int.Abs(xDistance) >= int.Abs(yDistance))
+				{
+					xChange = xDistance > 0 ? -1 : 1;
+				}
+				else
+				{
+					// we have farther to go on the y axis
+					yChange = yDistance > 0 ? -1 : 1;
+				}
+
+				xDistance += xChange;
+				yDistance += yChange;
+			}
 
 			return pathLength;
 		}
